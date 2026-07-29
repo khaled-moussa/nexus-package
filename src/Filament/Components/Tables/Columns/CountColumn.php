2b2e413fdcd8bdaa52ea6862e2b49190feb0801a
khaled-moussa <khaledmoussaeid@gmail.com>
2026-07-29 14:38:44 +0300

    refactor: Change formatUsing parameter type to callable and improve readability in CountColumn

--- src/Filament/Components/Tables/Columns/CountColumn.php
+++ src/Filament/Components/Tables/Columns/CountColumn.php
@@ -20,14 +20,19 @@ class CountColumn
         ?string $relation = null,
         bool $badge = true,
         TextSize $size = TextSize::Large,
-        ?Closure $formatUsing = null,
+        ?callable $formatUsing = null,
     ): TextColumn {
 
-        return TextColumn::make($name)
+        $column = TextColumn::make($name)
             ->size($size)
             ->when($label,    fn(TextColumn $column) => $column->label(__($label)))
             ->when($badge,    fn(TextColumn $column) => $column->badge())
-            ->when($relation, fn(TextColumn $column) => $column->counts($relation))
-            ->when($formatUsing, fn(TextColumn $column) => $column->formatStateUsing($formatUsing));
+            ->when($relation, fn(TextColumn $column) => $column->counts($relation));
+
+        if ($formatUsing) {
+            $column->formatStateUsing($formatUsing);
+        }
+
+        return $column;
     }
 }
